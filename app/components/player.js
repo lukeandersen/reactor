c4f4a8697ce3436aca6d64e4cd223857f7552752
Luke Andersen <luke@lawebdesign.com.au>
2016-07-11 21:00:23 +1000

    Get basic time working and make header table

--- app/components/player.js
+++ app/components/player.js
@@ -5,18 +5,11 @@ var Player = React.createClass({
     getInitialState() {
         return {
             playing: false,
-            pos: 0
+            pos: 0,
+            duration: 0
         };
     },
 	componentDidMount() {
-		// var wavesurfer = WaveSurfer.create({
-		//     container: '#screen',
-		//     waveColor: 'purple',
-		//     cursorColor: 'red'
-		// });
-        // setTimeout(() => {
-        //     wavesurfer.load(this.props.track.preview_url);
-        // }, 1000);
 	},
 	handleTogglePlay() {
         let playState = (this.state.playing === false) ? true : false;
@@ -24,12 +17,47 @@ var Player = React.createClass({
             playing: playState
         });
 	},
-	handlePause() {
-		wavesurfer.pause();
+	handleStop(e) {
+        console.log('stop');
+        // this.setState({
+        //     playing: false,
+        //     pos: 0
+        // });
+        e.wavesurfer.stop()
 	},
-	handleStop() {
-		wavesurfer.stop();
+    handleTempoChange() {
+        console.log('tempo changed', this.ref.tempo);
 	},
+    handleHotCue() {
+        let cues = [];
+        // TODO: get current payback pos
+        // this.setState({
+        //     playing: true,
+        //     pos: cues[0]
+        // });
+        console.log('hot cue');
+	},
+    handleLoopIn() {
+        let loop = [];
+        // TODO: store loop in and out
+        // TODO: make playback loop in <-> out range
+        // TODO: Exit loop
+        console.log('loop');
+	},
+    handlePosChange(e) {
+        let timeRemaining = function() {
+            return (e.wavesurfer.getDuration() - e.wavesurfer.getCurrentTime()).toFixed(2);
+        }
+        this.setState({
+            pos: e.originalArgs[0],
+            duration: timeRemaining()
+        });
+    },
+    playerReady(e) {
+        this.setState({
+            duration: e.wavesurfer.getDuration().toFixed(2)
+        });
+    },
     render() {
         const albumImg = {
             background: `url(${this.props.track.album})`
@@ -43,33 +71,35 @@ var Player = React.createClass({
                 <div className="header">
                     <div className="album" style={albumImg}></div>
                     <div className="header-body">
-                        <div className="clearfix">
-                            <h3 className="title">{this.props.track.name}</h3>
-                            <h3 className="time"></h3>
-                        </div>
-                        <div className="clearfix sub-title">
-                            <p className="artist">{this.props.track.artist}</p>
-                            <p className="pitch-val">0</p>
-                        </div>
+                        <table className="player-table">
+                            <tr>
+                                <td width="100%" className="title">{this.props.track.name}</td>
+                                <td width="90" className="time">-00.{this.state.duration}</td>
+                            </tr>
+                            <tr>
+                                <td className="artist muted">{this.props.track.artist}</td>
+                                <td className="pitch-val muted">0</td>
+                            </tr>
+                        </table>
                     </div>
                     <div className="deck">{this.props.name}</div>
                 </div>
                 <div className="body">
                     <div className="screen">
-                        <Wavesurfer audioFile={this.props.track.preview_url} playing={this.state.playing} options={waveOptions} />
+                        <Wavesurfer audioFile={this.props.track.preview_url} pos={this.state.pos} onPosChange={this.handlePosChange} onReady={this.playerReady} playing={this.state.playing} options={waveOptions} />
                     </div>
                     <div className="tempo">
-                        <input type="range" min="0" max="2" step="0.01" className="pitch slider slider-vertical" />
+                        <input type="range" ref="tempo" onChange={this.handleTempoChange} min="0" max="2" step="0.01" className="pitch slider slider-vertical" />
                     </div>
                 </div>
                 <div className="footer">
                     <div>
                         <button className="play btn btn-lg" onClick={this.handleTogglePlay}>Play</button>
-                        <button className="stop btn btn-lg">Stop</button>
+                        <button className="stop btn btn-lg" onClick={this.handleStop}>Stop</button>
                     </div>
 
                     <div className="hot-cue">
-                        <button className="btn">1</button>
+                        <button className="btn" onClick={this.handleHotCue}>1</button>
                         <button className="btn">2</button>
                         <button className="btn">3</button>
                         <button className="btn">4</button>
@@ -77,9 +107,9 @@ var Player = React.createClass({
 
                     <div className="loop">
                         <div className="btn-group">
-                            <button className="loop-in btn">In</button>
-                            <button className="loop-out btn">Out</button>
-                            <button className="loop-exit btn">Exit</button>
+                            <button className="loop-in btn" onClick={this.handleLoopIn}>In</button>
+                            <button className="loop-out btn" onClick={this.handleLoopOut}>Out</button>
+                            <button className="loop-exit btn" onClick={this.handleLoopExit}>Exit</button>
                         </div>
                     </div>
                 </div>
