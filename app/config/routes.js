b357ed050863126fd82148f0b20d8f709ba71eba
Luke Andersen <luke@lawebdesign.com.au>
2016-07-21 22:49:51 +1000

    Add static IP to test locally and update classes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {ReactRouter, Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {ReactRouter, Router, Route, IndexRoute, hashHistory} from 'react-router';
 import Main from '../components/main';
 import Home from '../components/home';
 
 var routes = (
-	<Router history={browserHistory}>
+	<Router history={hashHistory}>
 		<Route path='/' component={Main}>
 			<IndexRoute component={Home}></IndexRoute>
 		</Route>
